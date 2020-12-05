1a8510d0ef5066fc8381c6dd6a2c35c3589a2c1f
Marcin Medryk <medryk.kontakt@gmail.com>
2020-12-05 15:26:34 +0100

    x

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,13 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function Home() {
   return (
     <div>
-      Hello World
+      <div className="upBar"></div>
+      <img
+        className="quill"
+        src={process.env.PUBLIC_URL + "/Images/quill.svg"}
+        alt=""
+      ></img>
+      <div className="LogoText">Slight Bank</div>
       <Link to="/">Home</Link>
       <Link to="/Resources">Resources</Link>
       <Link to="/About">About</Link>

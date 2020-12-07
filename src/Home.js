0640efb80a8e416a254d98318f75bd0bffdd454d
Marcin Medryk <medryk.kontakt@gmail.com>
2020-12-07 18:52:59 +0100

    geez

--- src/Home.js
+++ src/Home.js
@@ -7,9 +7,24 @@ function Home() {
     <div>
       <Core />
       <div className="centre">
-        <div className="LoginWindow"></div>
-        <div className="LoginWindow"></div>
-        <div className="LoginWindow"></div>
+        <div className="LoginWindow">
+          <img
+            className="quillLogin"
+            src={process.env.PUBLIC_URL + "/Images/quill.svg"}
+            alt=""
+          ></img>
+          <div className="Title">Slight Bank</div>
+          <input className="input" />
+          <input className="input" />
+        </div>
+        <div className="space"></div>
+        <img
+          width="600px"
+          height="400px"
+          className="Freedom"
+          src={process.env.PUBLIC_URL + "/Images/freedom.jpg"}
+          alt=""
+        ></img>
       </div>
     </div>
   );

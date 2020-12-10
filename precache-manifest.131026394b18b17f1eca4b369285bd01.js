cda695c4da8fa3f4eaf30788521fec7965bc9f94
Marcin Medryk <medryk.kontakt@gmail.com>
2020-12-10 22:23:02 +0100

    Updates

---
+++ precache-manifest.131026394b18b17f1eca4b369285bd01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ea3ef14319f7cfc08d428ee8ba880a",
+    "url": "/SlightBank/index.html"
+  },
+  {
+    "revision": "edd063526f24c01b7e97",
+    "url": "/SlightBank/static/css/main.9e72216e.chunk.css"
+  },
+  {
+    "revision": "f0df564609188b692c47",
+    "url": "/SlightBank/static/js/2.50416eb9.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/SlightBank/static/js/2.50416eb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edd063526f24c01b7e97",
+    "url": "/SlightBank/static/js/main.d31a2406.chunk.js"
+  },
+  {
+    "revision": "ee2178a7453c79de980d",
+    "url": "/SlightBank/static/js/runtime-main.7fa8eb10.js"
+  }
+]);

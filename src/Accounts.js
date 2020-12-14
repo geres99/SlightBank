ce7a65c058a0adae68328a9c4989075741f8d8a6
Marcin Medryk <medryk.kontakt@gmail.com>
2020-12-14 22:41:40 +0100

    commit

--- src/Accounts.js
+++ src/Accounts.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Core from "./Core/Core";
+import "./Accounts.css";
 
 function Accounts() {
   return (
     <div>
       <Core />
+      <div>
+        <button className="showMoreButton">Accounts</button>
+        <button className="showMoreButton">Accounts made by user</button>
+      </div>
     </div>
   );
 }
--- src/useGlobalAccount.js
+++ src/useGlobalAccount.js
@@ -1,6 +1,11 @@
 import { createGlobalState } from "react-hooks-global-state";
 
-let initialState = { array: [["marcin", "marcin99"]] };
+let initialState = {
+  array: [
+    ["marcin", "marcin99"],
+    ["x", "x"],
+  ],
+};
 let { useGlobalState } = createGlobalState(initialState);
 
 export let useGlobalAccount = useGlobalState;

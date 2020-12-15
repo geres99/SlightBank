7b43e11975ecc57735afd63ec156839d8dd7f2ac
Marcin Medryk <medryk.kontakt@gmail.com>
2020-12-15 16:14:42 +0100

    FinalVersion

--- src/Accounts.js
+++ src/Accounts.js
@@ -4,16 +4,60 @@ import "./Accounts.css";
 import { useGlobalAccount } from "./useGlobalAccount";
 
 function Accounts() {
-  let [deafultAcc, setDefaultAcc] = React.useState([]);
-  let [userAcc, setUserAcc] = React.useState([]);
+  // eslint-disable-next-line no-unused-vars
   let [account, setAccount] = useGlobalAccount("array");
 
-  let showTextAbout = () => {
+  let userMadeAcc = account.slice(2);
+
+  let [deafultAcc, setDefaultAcc] = React.useState([]);
+  let [userAcc, setUserAcc] = React.useState([]);
+
+  let showTextDefault = () => {
     if (deafultAcc.length === 0) {
       setDefaultAcc(["x"]);
     } else {
       setDefaultAcc([]);
     }
+  };
+
+  let showTextUser = () => {
+    if (userAcc.length === 0) {
+      setUserAcc(["x"]);
+    } else {
+      setUserAcc([]);
+    }
+  };
+
+  let userAccShow = () => {
+    if (userMadeAcc.length !== 0) {
+      return (
+        <div>
+          <div className="bigText">User accounts:</div>
+          {userMadeAcc.map((x) => (
+            <div className="columnAccounts">
+              <div className="littleMargins centreAccounts">
+                There's a list of your accounts:
+              </div>
+              <div className="littleMargins centreAccounts">
+                Username: {x[0]}
+              </div>
+              <div className="centreAccounts"> Password: {x[1]}</div>
+            </div>
+          ))}
+        </div>
+      );
+    }
+    return (
+      <div>
+        <div className="bigText">User accounts:</div>
+        <div className="columnAccounts">
+          <div className="littleMargins centreAccounts">
+            List is empty, please create your account with Sign-up button in
+            home-page
+          </div>
+        </div>
+      </div>
+    );
   };
 
   let buttonStyle = (x) => {
@@ -41,7 +85,7 @@ function Accounts() {
     <div>
       <Core />
       <div>
-        <button onClick={showTextAbout} className="showButton">
+        <button onClick={showTextDefault} className="showButton">
           {buttonStyle(deafultAcc)}
           Default accounts:
         </button>
@@ -59,7 +103,11 @@ function Accounts() {
             </div>
           </div>
         ))}
-        <button className="showButton littleMargins">User accounts:</button>
+        <button onClick={showTextUser} className="showButton littleMargins">
+          {buttonStyle(userAcc)}
+          User accounts:
+        </button>
+        {userAcc.map((x) => userAccShow())}
       </div>
     </div>
   );
--- src/Resources.js
+++ src/Resources.js
@@ -2,9 +2,153 @@ import React from "react";
 import Core from "./Core/Core";
 
 function Resources() {
+  let [images, setImages] = React.useState([]);
+  let [icons, setIcons] = React.useState([]);
+
+  let showTextImages = () => {
+    if (images.length === 0) {
+      setImages(["x"]);
+    } else {
+      setImages([]);
+    }
+  };
+
+  let showTextIcons = () => {
+    if (icons.length === 0) {
+      setIcons(["x"]);
+    } else {
+      setIcons([]);
+    }
+  };
+
+  let buttonStyle = (x) => {
+    if (x.length === 0) {
+      return (
+        <img
+          height="30px"
+          className="nadropdown"
+          src={process.env.PUBLIC_URL + "/Images/drop-down-noactive.svg"}
+          alt=""
+        ></img>
+      );
+    }
+    return (
+      <img
+        height="30px"
+        className="nadropdown"
+        src={process.env.PUBLIC_URL + "/Images/drop-down-active.svg"}
+        alt=""
+      ></img>
+    );
+  };
+
   return (
     <div>
       <Core />
+      <button className="showButton" onClick={showTextImages}>
+        {buttonStyle(images)}
+        Images used:
+      </button>
+      {images.map((x) => (
+        <div>
+          <div className="bigText">Images used:</div>
+          <div className="smallText">
+            <div>
+              <div>Zdjęcie autorstwa Artem Beliaikin z Pexels</div>
+              <div className="littleMargins">
+                Zdjęcie autorstwa Vlada Karpovich z Pexels
+              </div>
+              <div className="littleMargins">
+                Zdjęcie autorstwa Pixabay z Pexels
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+      <button className="showButton" onClick={showTextIcons}>
+        {buttonStyle(icons)}
+        Icons used:
+      </button>
+      {icons.map((x) => (
+        <div>
+          <div className="bigText">Icons used:</div>
+          <div className="smallText">
+            <div>
+              <div>
+                Icons made by{" "}
+                <a
+                  href="https://www.flaticon.com/authors/google"
+                  title="Google"
+                >
+                  Google
+                </a>{" "}
+                from{" "}
+                <a href="https://www.flaticon.com/" title="Flaticon">
+                  www.flaticon.com
+                </a>
+              </div>
+              <div className="littleMargins">
+                Icons made by{" "}
+                <a
+                  href="https://www.flaticon.com/authors/pixel-perfect"
+                  title="Pixel perfect"
+                >
+                  Pixel perfect
+                </a>{" "}
+                from{" "}
+                <a href="https://www.flaticon.com/" title="Flaticon">
+                  www.flaticon.com
+                </a>
+              </div>
+              <div className="littleMargins">
+                <div>
+                  Icons made by{" "}
+                  <a
+                    href="https://www.flaticon.com/authors/freepik"
+                    title="Freepik"
+                  >
+                    Freepik
+                  </a>{" "}
+                  from{" "}
+                  <a href="https://www.flaticon.com/" title="Flaticon">
+                    www.flaticon.com
+                  </a>
+                </div>
+              </div>
+              <div className="littleMargins">
+                <div>
+                  Icons made by{" "}
+                  <a
+                    href="https://www.flaticon.com/authors/roundicons"
+                    title="Roundicons"
+                  >
+                    Roundicons
+                  </a>{" "}
+                  from{" "}
+                  <a href="https://www.flaticon.com/" title="Flaticon">
+                    www.flaticon.com
+                  </a>
+                </div>
+              </div>
+              <div className="littleMargins">
+                <div>
+                  Icons made by{" "}
+                  <a
+                    href="https://www.flaticon.com/authors/dmitri13"
+                    title="dmitri13"
+                  >
+                    dmitri13
+                  </a>{" "}
+                  from{" "}
+                  <a href="https://www.flaticon.com/" title="Flaticon">
+                    www.flaticon.com
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
